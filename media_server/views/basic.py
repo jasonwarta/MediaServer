48102a0cf6faa941547986d1c445f1af3569861d
Jason Warta <jasonwarta@gmail.com>
2017-07-27 15:34:31 -0800

    disabled paths that are no longer used

--- media_server/views/basic.py
+++ media_server/views/basic.py
@@ -42,55 +42,55 @@ def rescan_dir(folder=None):
 		return "success"
 	return "error"
 
-@app.route('/books')
-def books():
-	return render_template('views/books.html',files=get_books_list())
+# @app.route('/books')
+# def books():
+# 	return render_template('views/books.html',files=get_books_list())
 
-@app.route('/view_book/<fname>/view')
-def view_book(fname=None):
-	if fname is not None:
-		return render_template(
-			'views/view_book.html',
-			fname=fname
-		)
+# @app.route('/view_book/<fname>/view')
+# def view_book(fname=None):
+# 	if fname is not None:
+# 		return render_template(
+# 			'views/view_book.html',
+# 			fname=fname
+# 		)
 
-@app.route('/download/<group>/<path:fname>')
-def download(group=None,fname=None):
-	if group is not None and fname is not None:
-		dirs = {
-			'books'		: 'allitebooks/',
-			'tv' 		: 'tv/',
-			'movies'	: 'movies/' 
-		}
-		# r = requests.get(dirs[group]+fname, stream=True)
-		# return Response(r.iter_content(chunk_size=10*1024),
-			# content_type=r.headers['Content-Type'])
-		return Response(open('media_server/'+dirs[group]+fname), direct_passthrough=True)
-		# return send_file(
-			# dirs[group]+fname,
-			# as_attachment=True,
-			# attachment_filename=fname
-		# )
-		# 
+# @app.route('/download/<group>/<path:fname>')
+# def download(group=None,fname=None):
+# 	if group is not None and fname is not None:
+# 		dirs = {
+# 			'books'		: 'allitebooks/',
+# 			'tv' 		: 'tv/',
+# 			'movies'	: 'movies/' 
+# 		}
+# 		# r = requests.get(dirs[group]+fname, stream=True)
+# 		# return Response(r.iter_content(chunk_size=10*1024),
+# 			# content_type=r.headers['Content-Type'])
+# 		return Response(open('media_server/'+dirs[group]+fname), direct_passthrough=True)
+# 		# return send_file(
+# 			# dirs[group]+fname,
+# 			# as_attachment=True,
+# 			# attachment_filename=fname
+# 		# )
+# 		# 
 
-@app.route('/stream/<path:fname>')
-def stream(fname=None):
-	if fname is not None:
-		request_id = str(uuid4())
-		directory = 'media_server/streams/'+request_id
-		if not path.exists(directory):
-			os.makedirs(directory)
-		if not path.exists(directory+"/segments"):
-			os.makedirs(directory+"/segments")
-		p = subprocess.Popen(["media_server/generate_streams.sh",request_id,'media_server/'+fname])
-	return render_template(
-		'views/stream.html',
-		source="streams/"+request_id+"/stream.m3u8"
-	)
+# @app.route('/stream/<path:fname>')
+# def stream(fname=None):
+# 	if fname is not None:
+# 		request_id = str(uuid4())
+# 		directory = 'media_server/streams/'+request_id
+# 		if not path.exists(directory):
+# 			os.makedirs(directory)
+# 		if not path.exists(directory+"/segments"):
+# 			os.makedirs(directory+"/segments")
+# 		p = subprocess.Popen(["media_server/generate_streams.sh",request_id,'media_server/'+fname])
+# 	return render_template(
+# 		'views/stream.html',
+# 		source="streams/"+request_id+"/stream.m3u8"
+# 	)
 
-@app.route('/streams/<uuid>/stream.m3u8')
-def streams(uuid):
-	return send_file('streams/'+uuid+'/stream.m3u8')
+# @app.route('/streams/<uuid>/stream.m3u8')
+# def streams(uuid):
+# 	return send_file('streams/'+uuid+'/stream.m3u8')
 
 		
 @app.route('/search/<category>/',methods=['POST'])
